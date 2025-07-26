6a00deaac848054e3f37134976e666e72fd9868d
yanainaaa <yanainaferreir1@gmail.com>
2025-07-26 15:27:26 -0300

    Create subject.js

---
+++ subject.js
@@ -0,0 +1,184 @@
+[
+  {
+    "nombre": "Conceptos contables",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 1,
+    "requisitos": []
+  },
+  {
+    "nombre": "Administración y Gestión de las organizaciones 1",
+    "creditos": 10,
+    "area": "Administración",
+    "semestre": 1,
+    "requisitos": []
+  },
+  {
+    "nombre": "Introducción a la microeconomía",
+    "creditos": 10,
+    "area": "Economía",
+    "semestre": 1,
+    "requisitos": []
+  },
+  {
+    "nombre": "Calculo 1",
+    "creditos": 10,
+    "area": "Métodos cuantitativos",
+    "semestre": 1,
+    "requisitos": []
+  },
+  {
+    "nombre": "Contabilidad General 1",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 2,
+    "requisitos": ["Conceptos contables"]
+  },
+  {
+    "nombre": "Derecho Civil",
+    "creditos": 10,
+    "area": "Jurídica",
+    "semestre": 2,
+    "requisitos": []
+  },
+  {
+    "nombre": "Economía descriptiva",
+    "creditos": 10,
+    "area": "Economía",
+    "semestre": 2,
+    "requisitos": []
+  },
+  {
+    "nombre": "Introducción a la estadística",
+    "creditos": 10,
+    "area": "Economía",
+    "semestre": 3,
+    "requisitos": ["Calculo 1"]
+  },
+  {
+    "nombre": "Contabilidad General 2",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 3,
+    "requisitos": ["Contabilidad General 1"]
+  },
+  {
+    "nombre": "Derecho Comercial",
+    "creditos": 10,
+    "area": "Jurídica",
+    "semestre": 3,
+    "requisitos": ["Derecho Civil"]
+  },
+  {
+    "nombre": "Procesos y Sistemas de información",
+    "creditos": 10,
+    "area": "Administración",
+    "semestre": 3,
+    "requisitos": ["Administración y Gestión de las organizaciones 1"]
+  },
+  {
+    "nombre": "Contabilidad General 3",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 4,
+    "requisitos": ["Contabilidad General 2"]
+  },
+  {
+    "nombre": "Matematica financiera",
+    "creditos": 10,
+    "area": "Métodos cuantitativos",
+    "semestre": 4,
+    "requisitos": ["Calculo 1"]
+  },
+  {
+    "nombre": "Legislación laboral y seguridad social",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 4,
+    "requisitos": ["Conceptos contables", "Derecho Civil"]
+  },
+  {
+    "nombre": "Derecho societario",
+    "creditos": 10,
+    "area": "Jurídica",
+    "semestre": 4,
+    "requisitos": ["Derecho Civil"]
+  },
+  {
+    "nombre": "Modelos y sistemas de costos",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 5,
+    "requisitos": ["Contabilidad General 3"]
+  },
+  {
+    "nombre": "Derecho tributario",
+    "creditos": 10,
+    "area": "Jurídica",
+    "semestre": 5,
+    "requisitos": ["Derecho Comercial", "Derecho societario"]
+  },
+  {
+    "nombre": "Etica y ejercicio profesional",
+    "creditos": 5,
+    "area": "Actividades integradoras",
+    "semestre": 5,
+    "requisitos": ["Contabilidad General 2", "Derecho Comercial"]
+  },
+  {
+    "nombre": "Contabilidad superior 1",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 6,
+    "requisitos": ["Contabilidad General 3"]
+  },
+  {
+    "nombre": "Control interno",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 6,
+    "requisitos": ["Procesos y Sistemas de información"]
+  },
+  {
+    "nombre": "Contabilidad Gerencial",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 6,
+    "requisitos": ["Modelos y sistemas de costos", "Matematica financiera"]
+  },
+  {
+    "nombre": "Tributaria 1",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 6,
+    "requisitos": ["Contabilidad General 3", "Derecho tributario"]
+  },
+  {
+    "nombre": "Auditoría 1",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 7,
+    "requisitos": ["Contabilidad superior 1", "Legislación laboral y seguridad social", "Control interno", "Derecho societario"]
+  },
+  {
+    "nombre": "Tributaria 2",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 7,
+    "requisitos": ["Tributaria 1"]
+  },
+  {
+    "nombre": "Finanzas corporativas",
+    "creditos": 10,
+    "area": "Administración",
+    "semestre": 7,
+    "requisitos": ["Matematica financiera", "Introducción a la estadística", "Modelos y sistemas de costos"]
+  },
+  {
+    "nombre": "Informes profesionales de contador público",
+    "creditos": 10,
+    "area": "Contabilidad e impuestos",
+    "semestre": 8,
+    "requisitos": ["Auditoría 1"]
+  }
+]
